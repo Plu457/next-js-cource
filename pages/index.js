deed62a757770aa9ad41d8131c89986fecd94813
Pulan <ekgus5095@gmail.com>
2022-09-20 18:06:55 +0900

    Mini Project - dataFetching

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Link from 'next/link';
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
 
-function HomePage() {
+function HomePage({ events }) {
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
